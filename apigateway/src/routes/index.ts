81fbebd953333a4a70e4870ffc0d448898656788
Arison <arison646@gmail.com>
2024-11-18 19:07:39 +0100

    chore: adding our initial set up

---
+++ apigateway/src/routes/index.ts
@@ -0,0 +1,10 @@
+import  express  from "express";
+import cors from 'cors';
+import {createProxyMiddleware } from 'http-proxy-middleware';
+const router = express.Router();
+
+
+
+
+
+export default router;
