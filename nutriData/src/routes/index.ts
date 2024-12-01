c4e48469b7f0404fe8fb895235efdce8f0c41b06
Arison <arison646@gmail.com>
2024-12-01 14:46:37 +0100

    feat: adds controller for swapping meat

--- nutriData/src/routes/index.ts
+++ nutriData/src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import { searchFoodsOptionsByName } from "../controllers/nameSearchController";
 import { getNutritions } from "../controllers/getNutrionsController";
+import { getMeatlessSuggestionsKcal, getMeatlessSuggestionsProtein } from '../controllers/meatSwitchController'
 
 const router = express.Router();
 
@@ -13,6 +14,8 @@ router.get('/',(req,res,next) => {
 
 router.get('/search/:ingredient',cors(),searchFoodsOptionsByName)
 router.get('/getNutritions/:ingredient_code',cors(),getNutritions)
+router.get('/getSuggestions/protein/:ingredient_code',cors(),getMeatlessSuggestionsProtein)
+router.get('/getSuggestions/calories/:ingredient_code',cors(),getMeatlessSuggestionsKcal)
 
 
 export default router;
