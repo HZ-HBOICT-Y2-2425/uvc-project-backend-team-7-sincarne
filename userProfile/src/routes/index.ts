95bd83b9efbd5736768a0486a537702d71523f9d
georgyHZ <144170545+georgyHZ@users.noreply.github.com>
2025-01-03 17:21:38 +0200

    refactor: added comment in routes

--- userProfile/src/routes/index.ts
+++ userProfile/src/routes/index.ts
@@ -24,6 +24,8 @@ router.get("/getRecipe/:recipe_id", cors(), getRecipe);
 router.post("/createRecipe", [cors(),authUser], createRecipe);
 router.patch("/updateRecipe/:recipe_id", cors(), updateRecipe);
 router.delete("/deleteRecipe/:recipe_id", cors(), deleteRecipe);
+
+//Leaderboard routes
 router.get("/leaderboard", cors(), getLeaderboard);
 
 export default router;
