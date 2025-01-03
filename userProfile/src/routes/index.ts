b1edbdee20de2d1794a5b402eff000ee7f1ba01d
Georgy Furs <georgy.furs@gmail.com>
2025-01-03 17:17:03 +0200

    feat: co2stats backend implemented

--- userProfile/src/routes/index.ts
+++ userProfile/src/routes/index.ts
@@ -11,6 +11,7 @@ import {
 } from "../controllers/recipesController";
 import { updateDiary , getDiary} from "../controllers/diateryController";
 import { authUser } from "../middleware/authUser";
+import { getUserCO2, getTotalCO2 } from "../controllers/co2Controller";
 
 const router = express.Router();
 
@@ -32,5 +33,8 @@ router.post("/recipes/:recipe_id/addIngredient",cors(),addIngredient)
 router.patch("/updateRecipe/:recipe_id", cors(), updateRecipe);
 router.delete("/deleteRecipe/:recipe_id", cors(), deleteRecipe);
 
+// CO2-related routes
+router.get("/co2personal", [cors(), authUser], getUserCO2);
+router.get("/co2global", cors(), getTotalCO2);
 
 export default router;
